7a79db7ff0beaa9cb26e6a8743d569144e75129c
Carbon <robotcreeper13@gmail.com>
2020-08-17 11:38:13 -0500

    Implement NLog and setup Costura Fody (Keeps build dir clean).

--- ProjectTemplate/Program.cs
+++ ProjectTemplate/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Text;
+using NLog;
 
 namespace ProjectTemplate
 {
-    internal class Program
+    internal static class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            Console.WriteLine("A simple Project Template for my projects.");
+            logger.Info("A simple Project Template for my projects.");
+            logger.Info($"Global value : {Settings.Name}");
+            logger.Info($"Persistant value : {Settings.PSettings.PValue}");
         }
     }
 }
---
+++ ProjectTemplate/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Config.Net;
+namespace ProjectTemplate
+{
+    public static class Settings
+    {
+        //Global variables of any type
+        public static string Name = "Test Project";
+        
+        //Global persistent settings that are saved to a json file. Limited Types
+        public static IPersistentSettings PSettings = new ConfigurationBuilder<IPersistentSettings>().UseJsonFile("settings.json").Build();
+    }
+
+    
+    public interface IPersistentSettings
+    {
+        //Types allowed: bool, double, int, long, string, TimeSpan, DateTime, Uri, Guid
+        //DateTime is always converted to UTC
+        [Option(Alias = "SuperSecretStuff", DefaultValue = "Test")]
+        string PValue { get; set; }
+    }
+}
